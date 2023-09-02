db12c41b2c7381cd7b58bcf168acb5aa3d00b411
arttostog <arttostog@gmail.com>
2023-09-02 12:17:35 +0500

    Init Project

---
+++ src/test/java/me/arttostog/AuthorizationService/AuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.arttostog.AuthorizationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
